718f23d289c2c4cea4d589c1ac047c9d055dad8b
Adam <legioner005@gmail.com>
2025-04-02 18:21:10 +0400

    version 0.1

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
+import JournalForm from './components/JournalForm/JournalForm';
+import JournalList from './components/JournalList/JournalList';
+import Body from './layouts/Body/Body';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
+
+const INITIAL_DATA = [
+	// {
+	// 	id: 1,
+	// 	title: 'Подготовка к обновлению курсов',
+	// 	text: 'Горные походы открывают удивительные природные ландшафты',
+	// 	date: new Date(),
+	// },
+	// {
+	// 	id: 2,
+	// 	title: 'Поход в горы',
+	// 	text: 'Думал, что очень много времени',
+	// 	date: new Date(),
+	// },
+];
+
+function App() {
+	const [items, setItems] = useState(INITIAL_DATA);
+
+	const addItem = item => {
+		setItems(oldItems => [
+			...oldItems,
+			{
+				text: item.text,
+				title: item.title,
+				date: new Date(item.date),
+				id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1,
+			},
+		]);
+	};
+
+	return (
+		<div className="app">
+			<LeftPanel>
+				<Header />
+				<JournalAddButton />
+				<JournalList items={items} />
+			</LeftPanel>
+			<Body>
+				<JournalForm onSubmit={addItem} />
+			</Body>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import './Header.css';
+
+function Header({ children }) {
+	return <div className="logo">Personal Journal</div>;
+}
+
+export default Header;
